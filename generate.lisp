79b438fb10910d2f04dadb4f4548656194538925
Holy-Elie Scaide <hash@hescaide.me>
2022-10-24 20:30:22 -0400

    a more scripty style and some design changes

--- generate.lisp
+++ generate.lisp
@@ -1,124 +1,148 @@
 (in-package :cl-user)
 
 (defpackage :my-blog
-  (:use cl)
-  (:import-from :3bmd)
-  (:import-from :yaml)
-  (:import-from :access
-                :access
-                :accesses)
-  (:import-from :djula
-                :add-template-directory
-                :compile-template*
-                :render-template*)
+  (:use cl iterate spinneret)
   (:import-from :my-blog.parse-front-matter :parse)
-  (:export :generate))
-
+  (:export :generate-site))
 
 (in-package :my-blog)
 
-(setf *default-pathname-defaults* (uiop/os:getcwd))
+(defparameter *src-directory* (uiop/os:getcwd))
 
-(defparameter +config+ 
-  (with-open-file (in (merge-pathnames "config.yml"))
-    (yaml:parse (uiop:read-file-string in))))
+(defparameter *dest-directory* (merge-pathnames "dist/" *src-directory*))
 
-(defparameter +dist-directory+
-  (merge-pathnames (accesses +config+ "build" "directory")))
+(deftag my-header (control attrs)
+    `(:header
+	 (:nav :role "navigation"
+	       :aria-label "main-navigation"
+	  (:a :href "/" "about me")
+	  (:a :href "/portfolio.html" "portfolio")
+	  (:a :href "/notes.html" "notes")
+	  (:a :href "/media/cv.pdf" "resume"))))
+
+(defparameter *footer-content*
+  (with-open-file (in (merge-pathnames "templates/footer.html" *src-directory*))
+    (uiop:read-file-string in)))
+
+(deftag my-footer (control attrs)
+  `(:footer ,@control))
+
+(defmacro with-master (&body body)
+  `(with-html
+     (:doctype)
+     (:html
+      (:head
+       (:title "Hi, I'm Holy-Elie Scaide")
+       (:meta :name "viewport"
+	      :content "width=device-width, initial-scale=1, shrink-to-fit=no")
+       (:link :rel "stylesheet" :href "/style.css"))
+      (:body
+       (:main
+	(my-header)
+	(:article
+	 ,@body
+	 (my-footer (:raw ,*footer-content*))))))))
+
+(deftag page-header (control attrs)
+  `(:section.hero
+    (:p.title ,@control)))
+
+(defmacro with-page ((&key title) &body body)
+  `(with-master ()
+     (page-header ,title)
+     ,@body))
+
+(defparameter *index-content*
+  (with-open-file (in (merge-pathnames "templates/index.html" *src-directory*))
+    (uiop:read-file-string in)))
+
+(defun index-page ()
+  (with-master ()
+     (:raw *index-content*)
+    ))
+
+(defparameter *portfolio-content*
+  (with-open-file (in (merge-pathnames "templates/portfolio.html" *src-directory*))
+		  (uiop:read-file-string in)))
+
+(defun portfolio-page ()
+  (with-page (:title "Portfolio")
+    (:raw *portfolio-content*)))
+
+(defstruct note
+  title
+  dest-file
+  content
+  link)
+
+(defun notes-page (notes)
+  (with-page (:title "Notes")
+    (:p "Below are all my notes in no particular order.")
+    (:ul (dolist (note notes)
+	   (:li
+	    (:a :href (note-link note) (note-title note)))))))
+
+(defun note-page (note)
+  (with-page (:title (note-title note))
+    (:raw (note-content note))))
 
 
-(defparameter +template-directory+
-  (merge-pathnames (accesses +config+ "templates")))
+(defun get-note-dest-base (src)
+  (concatenate 'string "notes/" (pathname-name src) ".html"))
 
-(djula:add-template-directory +template-directory+)
+(defun get-note-link (base-path)
+  (concatenate 'string "/" base-path))
 
-(defparameter +notes-directory+ (merge-pathnames "notes/"))
+(defun find-notes ()
+  (let ((note-files (uiop:directory-files (merge-pathnames "notes/" *src-directory*))))
+    (iter (for note-file in note-files)
+      (collect (multiple-value-bind (fm-data markdown)
+		   (parse (uiop:read-file-string note-file))
+		 (let ((content (with-output-to-string (s)
+				  (3bmd:parse-string-and-print-to-stream markdown s)))
+		       (frontmatter (yaml:parse fm-data))
+		       (base-path (get-note-dest-base note-file)))
+		   (make-note :title (gethash "title" frontmatter "")
+			      :link (get-note-link base-path)
+			      :dest-file (merge-pathnames base-path *dest-directory*)
+			      :content content)))))))
 
-(defparameter +notes-dist-directory+ (merge-pathnames "notes/" +dist-directory+))
+(defparameter *static-files*
+  '("fonts/Charter Bold.otf"
+    "fonts/Charter Regular.otf"
+    "fonts/FiraMono-Regular.woff"
+    "media/cv.pdf"
+    "style.css"
+    "app.js"))
 
+(defun copy-files ()
+  (dolist (file *static-files*)
+    (let ((src (merge-pathnames file *src-directory*))
+	  (dest (merge-pathnames file *dest-directory*)))
+      (ensure-directories-exist dest)
+      (uiop:copy-file src dest))))
 
-(defparameter +content-types+ (access +config+ "content-types"))
+(defmacro write-page (dest &body body)
+  `(with-open-file (*html* ,dest
+			:direction :output
+			:if-exists :supersede
+			:if-does-not-exist :create)
+     ,@body))
 
-(defparameter +notes-html+ 
-  (compile-template* (accesses (first +content-types+) 
-                               "list"
-                               "template")))
+(defparameter *pages*
+  '(("index.html" index-page)
+    ("portfolio.html" portfolio-page)))
 
-(defparameter +note-html+ 
-  (compile-template* (accesses (first +content-types+) 
-                               "single"
-                               "template")))
-
-(defun ls-notes ()
-  (uiop:directory-files +notes-directory+))
-
-(defun render-note (file)
-  (multiple-value-bind (front content)
-      (parse (uiop:read-file-string file))
-    (values (with-output-to-string (s)
-              (3bmd:parse-string-and-print-to-stream content s))
-            (yaml:parse front)
-            file)))
-
-(defun dest-note-pathname (src)
-  (merge-pathnames (concatenate 'string (pathname-name src) ".html") +notes-dist-directory+))
-
-(defun write-note (content meta file)
-  (with-open-file (in (dest-note-pathname file) 
-                      :direction :output
-                      :if-does-not-exist :create
-                      :if-exists :overwrite)
-    (render-template* +note-html+ in 
-                      :content content 
-                      :title (gethash "title" meta ""))))
-
-(defun get-notes ()
-  (mapcar (lambda (file) (multiple-value-list (render-note file))) (ls-notes)))
-
-(defun note-link (file)
-  (concatenate 'string "./notes/" (pathname-name file) ".html"))
-
-(defun note-for-template (note-as-list)
-  (let ((note (make-hash-table)))
-    (setf (access note :title) (access (second note-as-list) "title"))
-    (setf (access note :link) (note-link (third note-as-list)))
-    note))
-
-(defun write-notes ()
-  (let ((notes (get-notes)))
-    (with-open-file (in (merge-pathnames "notes.html" +dist-directory+)
-                        :direction :output
-                        :if-does-not-exist :create
-                        :if-exists :overwrite)
-      (render-template* +notes-html+ 
-                        in 
-                        :notes (mapcar #'note-for-template notes)))
+(defun generate-site ()
+  (ensure-directories-exist *dest-directory*)
+  (dolist (p *pages*)
+    (write-page (merge-pathnames (first p) *dest-directory*)
+      (apply (second p) nil)))
+  (let ((notes (find-notes)))
+    (write-page (merge-pathnames "notes.html" *dest-directory*)
+      (notes-page notes))
     (dolist (note notes)
-      (destructuring-bind (content meta file) note
-          (write-note content meta file)))))
-
-
-(defun write-page (page)
-  (with-open-file (in (merge-pathnames page +dist-directory+)
-                      :direction :output
-                      :if-does-not-exist :create
-                      :if-exists :overwrite)
-    (render-template* (compile-template* page) in)))
-
-(defun build-path (path)
-  (concatenate 'string (accesses +config+ "build" "directory") path))
-
-(defun copy-files () 
-    (dolist (file (access +config+ "static-files"))
-      (let ((src (access file "src"))
-            (dest (build-path (access file "dest"))))
-        (ensure-directories-exist dest)
-        (uiop:copy-file src dest))))
-
-(defun generate ()
-  (ensure-directories-exist +dist-directory+)
-  (dolist (page '("index.html" "portfolio.html"))
-    (write-page page))
-  (ensure-directories-exist +notes-dist-directory+)
-  (write-notes)
+      (ensure-directories-exist (note-dest-file note))
+      (write-page (note-dest-file note)
+	(note-page note))))
   (copy-files))
--- my-blog.asd
+++ my-blog.asd
@@ -2,11 +2,11 @@
   :version "0.1.0"
   :author "Holy-Elie Scaide"
   :depends-on ("3bmd"
-               "djula"
-               "access"
-               "cl-yaml")
+	       "iterate"
+	       "spinneret"
+	       "cl-yaml")
   :license "MIT"
   :components ((:file "generate" :depends-on ("parse-front-matter"))
-               (:file "parse-front-matter")))
+	       (:file "parse-front-matter")))
 
 ; vim: ft=lisp
